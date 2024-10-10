b242e1792aff2431758b4e33b0735a2b2b88e8d1
Uchida Mizuki <uchidamizuki@vivaldi.net>
2024-10-10 23:48:14 +0900

    Use FixedPoint in goods_reprice_recursively()

--- R/goods-reprice.R
+++ R/goods-reprice.R
@@ -3,34 +3,20 @@
 #' @param data A `econ_goods` object.
 #' @param prices A data frame or a list of data frames that contains columns
 #' `price`.
-#' @param gradient Logical input to return the gradient. By default, `FALSE`.
 #'
 #' @return A `econ_goods` object.
 #'
 #' @export
 goods_reprice <- function(data,
-                          prices = NULL,
-                          gradient = FALSE) {
-  if (gradient) {
-    data <- data |>
-      dplyr::mutate(gradient = vctrs::vec_init(list()))
-  }
-
+                          prices = NULL) {
   if (is.data.frame(prices)) {
     prices <- list(prices)
   }
 
   for (i in seq_along(prices)) {
-    if (gradient) {
-      prices[[i]] <- prices[[i]] |>
-        tibble::add_column(gradient = prices[[i]] |>
-                             dplyr::mutate(price = 1) |>
-                             vctrs::vec_chop())
-    }
-
     data <- data |>
       dplyr::rows_update(prices[[i]],
-                         by = setdiff(names(prices[[i]]), c("price", "gradient")))
+                         by = setdiff(names(prices[[i]]), "price"))
   }
 
   data |>
@@ -38,27 +24,6 @@ goods_reprice <- function(data,
       quantities <- util_demand(x$utility[[1]], y$price,
                                 utility = x$quantity)
       x$price <- sum(y$price * quantities) / x$quantity
-
-      if (gradient) {
-        gradient_quantities <- util_demand(x$utility[[1]], y$price,
-                                           utility = x$quantity,
-                                           gradient = TRUE)
-        gradient_new <- as.double(gradient_quantities %*% y$price) / x$quantity +
-          quantities / x$quantity
-
-        x$gradient[[1]] <- purrr::map2(
-          gradient_new, y$gradient,
-          \(gradient_new, gradient) {
-            if (vctrs::vec_is_empty(gradient)) {
-              NULL
-            } else {
-              gradient |>
-                dplyr::mutate(price = .data$price * .env$gradient_new)
-            }
-          }
-        ) |>
-          dplyr::bind_rows()
-      }
       x
     })
 }
@@ -67,49 +32,22 @@ goods_reprice <- function(data,
 #'
 #' @param data A `econ_goods` object.
 #' @param f A function that returns a data frame with columns `price`.
-#' @param gradient A function that returns a data frame with columns `gradient`.
 #' @param ... Additional arguments passed to `stats::optim()`.
 #'
 #' @return A `econ_goods` object.
 #'
 #' @export
-goods_reprice_recursively <- function(data, f,
-                                      gradient = NULL, ...) {
+goods_reprice_recursively <- function(data, f, ...) {
   prices <- f(data)
 
-  par <- prices$price
-  fn <- function(par) {
+  price <- prices$price
+  reprice <- function(par) {
     prices$price <- par
     data <- goods_reprice(data, prices)
-    prices_new <- f(data)
-
-    sum((prices_new$price - par) ^ 2)
+    f(data)$price
   }
-  if (is.null(gradient)) {
-    gr <- NULL
-  } else {
-    gr <- function(par) {
-      prices$price <- par
-      data <- goods_reprice(data, prices,
-                            gradient = TRUE)
-      prices_new <- f(data) |>
-        dplyr::left_join(gradient(data),
-                         by = setdiff(names(prices), "price"))
-
-      purrr::map2_dbl(
-        prices_new$price - par, prices_new$gradient,
-        \(change_price, gradient) {
-          2 * sum(change_price * (vctrs::vec_size(gradient) * gradient$price - 1))
-        }
-      )
-    }
-  }
-  par <- stats::optim(par = par,
-                      fn = fn,
-                      gr = gr,
-                      method = "BFGS", ...) |>
-    purrr::chuck("par")
-
-  prices$price <- par
+  price <- FixedPoint::FixedPoint(reprice, price, ...) |>
+    purrr::chuck("FixedPoint")
+  prices$price <- price
   goods_reprice(data, prices)
 }
--- tests/testthat/test-goods-reprice.R
+++ tests/testthat/test-goods-reprice.R
@@ -1,29 +1,11 @@
-test_that("goods_reprice() works", {
-  set.seed(1234)
-
-  industry <- get_industry_iotable_regional(iotable = get_iotable_dummy())
-  prices <- get_prices_industry_iotable_regional(industry)
-
-  analytic <- industry |>
-    goods_reprice(prices,
-                  gradient = TRUE) |>
-    tibble::as_tibble() |>
-    dplyr::select(!c("price", "quantity", "utility"))
-  numerical <- goods_reprice_gradient_numerical(goods = industry,
-                                                prices = prices)
-
-  expect_equal(analytic, numerical,
-               tolerance = 1e-6)
-})
-
 test_that("goods_reprice_recursively() works", {
   set.seed(1234)
 
   industry <- get_industry_iotable_regional(iotable = get_iotable_dummy())
 
   prices <- get_prices_industry_iotable_regional(industry) |>
-    dplyr::filter(.data$input_sector %in% c("a", "b"),
-                  .data$output_sector %in% c("a", "b")) |>
+    dplyr::filter(.data$input_sector %in% c("industry_1", "industry_2"),
+                  .data$output_sector %in% c("industry_1", "industry_2")) |>
     dplyr::mutate(price = runif(dplyr::n()))
 
   f <- function(industry) {
@@ -36,21 +18,9 @@ test_that("goods_reprice_recursively() works", {
       dplyr::left_join(prices_new,
                        by = "output_sector")
   }
-  gradient <- function(industry) {
-    prices_new <- industry |>
-      tibble::as_tibble() |>
-      dplyr::select("output_sector", "gradient")
-
-    prices |>
-      dplyr::select(!"price") |>
-      dplyr::left_join(prices_new,
-                       by = "output_sector")
-  }
-
   industry_new <- industry |>
     goods_reprice(prices) |>
-    goods_reprice_recursively(f,
-                              gradient = gradient)
+    goods_reprice_recursively(f)
 
   expect_equal(industry_new, industry,
                tolerance = 1e-6)
