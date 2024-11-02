0dce8ac9908c7a69a674733573b196485662f189
Uchida Mizuki <uchidamizuki@vivaldi.net>
2024-11-02 11:54:04 +0900

    Update `goods_by()` and `goods_compose()`

--- R/goods.R
+++ R/goods.R
@@ -1,12 +1,18 @@
 #' Create goods
 #'
-#' @param .data A data frame that contains columns `price` and `quantity`.
+#' @param .data A data frame that contains columns `price` and `quantity`. If
+#' `price` is not available, it will be created with a value of 1.
 #' @param ... Variables to group by.
 #'
 #' @return A `econ_goods` object.
 #'
 #' @export
 goods_by <- function(.data, ...) {
+  if (!"price" %in% names(.data)) {
+    .data <- .data |>
+      tibble::add_column(price = 1)
+  }
+
   .data |>
     timbr::forest_by(...) |>
     dplyr::select("price", "quantity") |>
@@ -16,7 +22,8 @@ goods_by <- function(.data, ...) {
 #' Compose goods
 #'
 #' @param data A `econ_goods` object.
-#' @param utility A `econ_util` object.
+#' @param utility A `econ_util` object or a data frame that contains columns
+#' `utility`.
 #' @param node A node name for composition goods. By default, it is `NULL`.
 #'
 #' @return A `econ_goods` object.
@@ -24,17 +31,25 @@ goods_by <- function(.data, ...) {
 #' @export
 goods_compose <- function(data, utility,
                           node = NULL) {
-  data |>
+  data <- data |>
     dplyr::summarise(prices = .data$price |>
                        rlang::set_names(timbr::node_value()) |>
                        list(),
                      quantities = .data$quantity |>
                        rlang::set_names(timbr::node_value()) |>
                        list(),
-                     .node = node) |>
-    dplyr::mutate(utility = ifelse(inherits(.env$utility, "econ_util"),
-                                   list(.env$utility),
-                                   unname(.env$utility[timbr::node_value()]))) |>
+                     .node = node)
+
+  if (inherits(utility, "econ_util")) {
+    data <- data |>
+      dplyr::mutate(utility = list(.env$utility))
+  } else {
+    data <- data |>
+      dplyr::rows_update(utility,
+                         by = setdiff(names(utility), "utility"))
+  }
+
+  data |>
     dplyr::mutate(
       utility = list(.data$utility, .data$prices, .data$quantities) |>
         purrr::pmap(\(utility, prices, quantities) {
